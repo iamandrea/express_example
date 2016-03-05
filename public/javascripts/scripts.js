e8a827611e5793352c1ecfac4ed3e617683b5998
Andrea Lizard <a.agathocleous92@gmail.com>
2016-03-05 16:29:04 +1100

    Updates

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -96,15 +96,6 @@ jQuery(function($){
 		  slidesToShow: 1
 	});
 
-	$('.buttons').slick({
-		  dots: true,
-		  infinite: true,
-		  speed: 300,
-		  slidesToShow: 1,
-		  centerMode: true,
-		  slidesToShow: 1
-	});
-		
 
 });
 
